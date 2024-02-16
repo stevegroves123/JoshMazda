30f0f41780bef88936e4d2762e4b7b10baf86b88
Steve Groves <stevegroves123@gmail.com>
2024-02-16 15:04:45 +0000

    upload of basic Arduino sketch
    Two files to test the relays on the Arduino Relay Shield

---
+++ TestLEDsOneAtATime/TestLEDsOneAtATime.ino
@@ -0,0 +1,36 @@
+// the setup function runs once when you press reset or power the board
+relay1 = 4;
+relay2 = 7;
+relay3 = 8;
+relay4 = 12;
+
+void setup() {
+  // initialize digital pins to output to the RELAYs.
+  pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+  pinMode(relay3, OUTPUT);
+  pinMode(relay4, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(relay1, HIGH);  // turn relay1 LED on (HIGH is the voltage level)
+  delay(500);                  // wait half a second
+  digitalWrite(relay1, LOW);  // turn relay1 LED on (LOW is the voltage level)
+  delay(500);
+
+  digitalWrite(relay2, HIGH);  // turn relay2 LED on
+  delay(500);                 
+  digitalWrite(relay2, LOW);  // turn relay2 LED off
+  delay(500);
+
+  digitalWrite(relay3, HIGH);  // turn relay3 LED on
+  delay(500);                 
+  digitalWrite(relay3, LOW);  // turn relay3 LED off
+  delay(500);
+
+  digitalWrite(relay4, HIGH);  // turn relay4 on
+  delay(500);                  
+  digitalWrite(relay4, LOW);   // turn relay4 off
+  delay(500); 
+}
